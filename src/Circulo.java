cc3dd3fb565221a660422394903ae14df5cd1aeb
jonvicente10 <jonvicente10@hotmail.com>
2014-09-29 14:24:12 +0200

    ahora funciona con get y set

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,20 +1,42 @@
 /* Clase para calcular el area y la circunferencia de un circulo */
 
-public class Circulo {
- 
-//Metodo que calcula el area y devuelve el resultado.
- 
-public double area (double radio) {
+	public class Circulo {
 
-	double result = 3.1416 * Math.pow(radio,2);
-	return result;
- }
 
-//Metodo que calcula la circunferencia y devuelve el resultado.
-public double circunferencia (double radio) {
+		/* Se declaran las variables publicas radio, area y circunferencia */
+		private double radio;
+		private double area;
+		private double circunferencia;
 
-	double result = 2 * 3.1416 * radio;
-	return result;
- }
 
-}
+		/* Metodo para dar valor a radio mediante el dato introducido */
+		public void setRadio (double radiointroducido)
+ 		{
+			radio = radiointroducido;
+ 		}
+		
+		/* Metodo para obtener (get) el valor de la variable radio */
+		public double getRadio()
+ 		{
+			return radio;
+ 		}
+
+
+		/* Metodo para calcular y establecer el valor del area teniendo en cuenta el valor introducido */
+		public double area()
+ 		{
+			double result;
+			result = 3.1416 * Math.pow(this.radio,2);
+			return result;
+		}
+
+
+		/* Metodo para calcular y establecer el valor de la circunferencia teniendo en cuenta el valor introducido */
+		public double circunferencia()
+ 		{
+			double result;
+			result = 2 * 3.1416 * this.radio;
+			return result;
+ 		}
+
+	}
